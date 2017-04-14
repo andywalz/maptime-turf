cd1f9fa30cdb70e2e143309d58cff36b2187f77c
Andy Walz <andywalz@gmail.com>
2017-04-14 00:47:59 -0500

    Bug fix, more comments, and error handling added

--- index.js
+++ index.js
@@ -7,62 +7,95 @@
 console.log("Hello MaptimeMSP");
 
 // Instantiate packages
+const request = require('request');   // https://www.npmjs.com/package/request
 const GeoJSON = require('geojson');   // https://www.npmjs.com/package/geojson
-const getJSON = require('get-json');  // https://www.npmjs.com/package/get-json
 const fs = require('fs');
 const open = require('open');
 const turf = require('@turf/turf');
 
-// Fetch Nice Ride MN Station Locations as GBFS
-getJSON('https://api-core.niceridemn.org/gbfs/en/station_information.json', function(error, response){
 
-    // Convert GBFS to GeoJSON
-    var stations = GeoJSON.parse(response.data.stations, {Point: ['lat', 'lon'],include: ['name']});
+// Configure the request for Nice Ride JSON feed.
+// Note: I had to switch from get-json to request because the API started
+// rejecting requests without user-agent header, which request supports
+var options = {
+    url: 'https://api-core.niceridemn.org/gbfs/en/station_information.json',
+    method: 'GET',
+    headers: {
+        'User-Agent':       'Super Agent/0.0.1'
+    }
+}
 
-    // Map stations in geojson.io
-    //mapit ( stations );
+// Execute request to NiceRide API
+request(options, function (error, response, body){
 
-    // read in geojson
-    const libraries = JSON.parse(fs.readFileSync('./data/mn_libraries.geojson')); // feature collection of points
-    const aoi = JSON.parse(fs.readFileSync('./data/hennepin.geojson')); // Area of Interest: Hennepin Co
+    // You're now in the callback function which means data received or error occured
 
-    var mplsStations = turf.within(stations, aoi); // Return only libraries within Hennepin co
+    if (!error && response.statusCode === 200) {
 
-    //mapit ( mplsStations );
+        // No errors, proceed by converting JSON text to JavaScript object
+        const stationJson = JSON.parse(body);
 
-    // Write output to .geojson file, open in QGIS
-    // (NOTE: must set QGIS as default program for all .geojson files)
-    fs.writeFile('./data/mplsStations.geojson',JSON.stringify(mplsStations),function(err){
-      open('./data/mplsStations.geojson');
-    });
+        // Convert Bikeshare Format to GeoJSON (https://www.npmjs.com/package/geojson)
+        var stations = GeoJSON.parse(stationJson.data.stations, {Point: ['lat', 'lon'],include: ['name']});
 
-    // Buffer bike stations by ~2 blocks
-    var polygons = turf.buffer(mplsStations,0.2,'kilometers');
+        // Map stations in geojson.io
+        mapit ( stations );
 
-    fs.writeFile('./data/buffered_stations.geojson',JSON.stringify(polygons),function(err){
-      open('./data/buffered_stations.geojson');
-    });
+        // Now let's read in geojson (feature collection of points) from a file
+        const libraries = JSON.parse(fs.readFileSync('./data/mn_libraries.geojson'));
 
-    // Get libraries that fall within buffered Nice Ride stations
-    var librariesWithNice = turf.within(libraries, polygons);
+        // Let's not try to map libraries (this is what caused the bug during demo--the dataset is just too large to pass via url to geojson.io)
+        // Actually you can try it now if you want because I added error handling to mapit() see below
+        //mapit ( libraries );
 
-    fs.writeFile('./data/librariesWithNice.geojson',JSON.stringify(librariesWithNice),function(err){
-      open('./data/librariesWithNice.geojson');
-    });
+        // Also read in area of interest: Hennepin Co
+        const aoi = JSON.parse(fs.readFileSync('./data/hennepin.geojson'));
 
-    fs.writeFile('./data/allLibraries.geojson',JSON.stringify(libraries),function(err){
-      open('./data/allLibraries.geojson');
-    });
+        // Finally some Turf.js - Return only libraries within Hennepin Co
+        var mplsStations = turf.within(stations, aoi);
 
+        // Write output to .geojson file, open in QGIS
+        // (NOTE: must set QGIS as default program for all .geojson files)
+        fs.writeFile('./data/mplsStations.geojson',JSON.stringify(mplsStations),function(err){
+          open('./data/mplsStations.geojson');
+        });
+
+        // Buffer bike stations by ~2 blocks
+        var polygons = turf.buffer(mplsStations,0.2,'kilometers');
+
+        // Write buffered stations file, open in QGIS
+        fs.writeFile('./data/buffered_stations.geojson',JSON.stringify(polygons),function(err){
+          open('./data/buffered_stations.geojson');
+        });
+
+        // Get libraries that fall within buffered Nice Ride stations
+        var librariesWithNice = turf.within(libraries, polygons);
+
+        // Open all libaries and only libraries with Nice Ride stations in QGIS
+        fs.writeFile('./data/librariesWithNice.geojson',JSON.stringify(librariesWithNice),function(err){
+          open('./data/librariesWithNice.geojson');
+        });
+        fs.writeFile('./data/allLibraries.geojson',JSON.stringify(libraries),function(err){
+          open('./data/allLibraries.geojson');
+        });
+
+
+    } else {
+        console.log("Got an error: ", error, response, ", status code: ", response.statusCode)
+    }
 });
 
 
 function mapit(geojson) {
     // open package allows open a file or url in the user's preferred application
-    const open = require('open');
+
+    try {
     // pass data to geojson.io via url segment
-    var geojsonUrl = 'http://geojson.io/#data=data:application/json,' + encodeURIComponent(JSON.stringify(geojson))    // open map in default web browser
-    open(geojsonUrl);
+        var geojsonUrl = 'http://geojson.io/#data=data:application/json,' + encodeURIComponent(JSON.stringify(geojson))    // open map in default web browser
+        open(geojsonUrl);
+    } catch(err) {
+        console.log("ERROR: Dataset too large! Can't open via geojson.io url. Try using QGIS");
+    }
 }
 
 
